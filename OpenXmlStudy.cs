8c833ce21e4a431365a68b178afbb9969dcb432c
fabianvalverde <110683318+fabianvalverde@users.noreply.github.com>
2022-09-24 04:12:59 -0600

    Practice Console Project Created

---
+++ OpenXmlStudy.cs
@@ -0,0 +1,8 @@
+using System.Security.AccessControl;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Linq;
+using System.Collections.Generic;
+
+Console.WriteLine("Open XML Practice");
